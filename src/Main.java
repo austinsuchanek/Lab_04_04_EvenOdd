885dd59e21ef50bb121fce15611208d80e86c76b
austinsuchanek <suchanad@mail.uc.edu>
2022-09-18 12:16:45 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        int vnum = 3;
+
+
+
+        if(vnum % 2 == 0)
+           System.out.println(vnum + " is even");
+        else
+        System.out.println(vnum + " is odd");
+
+
+    }
+
+}
